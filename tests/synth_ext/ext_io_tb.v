a16a947eeb570e10b96f35aef1358dc5ad4f82fd
Yusuke Tabata <tabata.yusuke@gmail.com>
2017-12-17 22:07:47 +0900

    Add a test of ext-io.

---
+++ tests/synth_ext/ext_io_tb.v
@@ -0,0 +1,24 @@
+// This script is invoked from run-test script
+`timescale 1ns/1ns
+
+module ext_io_tb;
+
+   reg clk, rst;
+   wire [3:0] led;
+
+   initial begin
+      clk <= 0;
+      rst <= 1;
+      #105;
+      rst <= 0;
+      $display("test_tb done");
+      $finish;
+   end
+
+   always begin
+      #10 clk = ~clk;
+   end
+
+   mod_main m(.clk(clk), .rst(rst), .led(led), .in(led));
+
+endmodule // ext_io_tb
