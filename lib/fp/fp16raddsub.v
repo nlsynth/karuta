dfe19e84330949c3d859f79de2840ee252aab927
Yusuke Tabata <tabata.yusuke@gmail.com>
2018-04-20 09:18:09 +0900

    5stages FP16R adder/subtractor (still needs test).

--- lib/fp/fp16raddsub.v
+++ lib/fp/fp16raddsub.v
@@ -1,146 +1,265 @@
 // FP16 Regular/Relaxed version.
 // TODO: Finish the implementation.
 // TODO: Write tests and fix obvious bugs first.
-module FP16RAddSubS0Of4(
+module FP16RAddSubS0Of5(
  input 	       clk,
  input 	       rst,
  input [15:0]  arg_0,
  input [15:0]  arg_1,
  input 	       arg_2,
- output [15:0] ret_0, // lhs
- output [15:0] ret_1, // rhs
- output        ret_2, // is_sub
- output        ret_3, // neg_lhs
- output        ret_4  // neg_rhs
-);
+ output [15:0] ret_0,
+ output [15:0] ret_1,
+ output        ret_2,
+ output        ret_3);
 
+   wire [15:0] x;
+   wire [15:0] y;
    wire        is_sub;
-   wire [4:0]  e0;
-   wire [4:0]  e1;
-   wire        s0;
-   wire        s1;
-   wire        swap;
+   wire        ys;
+   wire        yys;
+   wire [15:0] yy;
    wire        diff_sign;
-
-   assign is_sub = arg_2;
-   assign e0 = arg_0[14:10];
-   assign e1 = arg_0[14:10];
-   assign s0 = arg_0[15];
-   assign s1 = arg_0[15];
-   assign diff_sign = s0 ^ s1;
-   assign swap = e0 < e1;
-
-   assign ret_0 = swap ? arg_1 : arg_0;
-   assign ret_1 = swap ? arg_0 : arg_1;
-   assign ret_2 = arg_2;
-   // neg lhs
-   assign ret_3 = (swap & is_sub) || (swap && !is_sub && diff_sign && s1);
-   // neg rhs
-   assign ret_4 = (!swap & is_sub) || (swap && !is_sub && diff_sign && s0);
+   wire [4:0]  xe;
+   wire [4:0]  ye;
+   wire        swap;
+   wire        neg_lhs;
+   wire        neg_rhs;
+   wire [15:0] lhs;
+   wire [15:0] rhs;
    
-endmodule // FP16RAddSubS0Of4
 
-module FP16RAddSubS1Of4(
+   assign x = arg_0;
+   assign y = arg_1;
+   assign is_sub = arg_2;
+   assign ys = y[15];
+   assign yys = is_sub ? ~ys : ys;
+   assign yy = {yys, y[14:0]};
+   assign xe = x[14:10];
+   assign ye = y[14:10];
+
+   assign diff_sign = x[15] ^ yy[15];
+
+   assign swap = xe < ye;
+   assign lhs = swap ? yy : x;
+   assign rhs = swap ? x : yy;
+
+   assign neg_lhs = diff_sign ? lhs[15] : 0;
+   assign neg_rhs = diff_sign ? rhs[15] : 0;
+
+   assign ret_0 = lhs;
+   assign ret_1 = rhs;
+   assign ret_2 = neg_lhs;
+   assign ret_3 = neg_rhs;
+
+endmodule // FP16RAddSubS0Of5
+
+module FP16RAddSubS1Of5(
  input 	       clk,
  input 	       rst,
- input [15:0]  arg_0, // LHS
- input [15:0]  arg_1, // RHS
- input 	       arg_2, // is_sub
- input 	       arg_3, // neg_lhs
- input 	       arg_4, // neg_rhs
- output [11:0] ret_0, // xf
- output [20:0] ret_1, // yf
- output [4:0]  ret_2, // xe
- output        ret_3  // is_sub
-);
+ input [15:0]  arg_0,
+ input [15:0]  arg_1,
+ input 	       arg_2,
+ input 	       arg_3,
+ output        ret_0,
+ output        ret_1,
+ output [21:0] ret_2,
+ output [21:0] ret_3,
+ output [4:0]  ret_4,
+ output        ret_5,
+ output        ret_6);
 
-   wire [4:0] xe;
-   wire [10:0] xf;
-   wire        x1;
+   wire [15:0] x;
+   wire [15:0] y;
+   wire        xn;
+   wire        yn;
+   wire [4:0]  xe;
    wire [4:0]  ye;
-   wire [10:0] yf;
+   wire        x1;
    wire        y1;
-   wire [20:0] yr0;
-   wire        is_sub;
+   wire [9:0]  xf;
+   wire [9:0]  yf;
+   wire [20:0] xr;
+   wire [20:0] xrn;
+   wire [10:0] yr10;
    wire [4:0]  d;
    wire [20:0] yr1;
    wire [20:0] yr2;
    wire [20:0] yr4;
    wire [20:0] yr8;
-   wire [20:0] yr16;
+   wire [20:0] yr16;   
+   wire [20:0] yrn;
+
+   assign x = arg_0;
+   assign y = arg_1;
+   assign xn = arg_2;
+   assign yn = arg_3;
+   assign xe = x[14:10];
+   assign ye = y[14:10];
+   assign xf = x[9:0];
+   assign yf = y[9:0];
    
-   assign xe = arg_0[4:0];
-   assign ye = arg_1[4:0];
-   assign x1 = (xe != 0);
-   assign y1 = (xe != 0);
-   assign xf = {x1,arg_0[9:0]};
-   assign yf = {y1,arg_1[9:0]};
+   assign x1 = xe > 0;
+   assign y1 = ye > 0;
+   assign xr = {x1, xf, 10'b0};
+   assign xrn = xn ? ~xr : xr;
+   assign yr10 = {y1, yf};
    assign d = xe - ye;
-   assign yr0 = {yf, 10'b0};
-   assign is_sub = arg_2;
-   // barrel shifter.
-   assign yr1 = d[0:0] ? {1'b0, yr0[20:1]} : yr0;
+   // barrel shifter to adjust y.
+   assign yr1 = d[0:0] ? {1'b0, yr10[20:1]} : yr10;
    assign yr2 = d[1:1] ? {2'b0, yr1[20:2]} : yr1;
    assign yr4 = d[2:2] ? {4'b0, yr2[20:4]} : yr2;
    assign yr8 = d[3:3] ? {8'b0, yr4[20:8]} : yr4;
    assign yr16 = d[4:4] ? {16'b0, yr8[20:16]} : yr8;
 
-   assign ret_0 = arg_3 ? xf : ~xf;
-   assign ret_1 = arg_4 ? yr16 : ~yr16;
-   assign ret_2 = xe;
-   assign ret_3 = is_sub;
+   assign yrn = yn ? ~yr16 : yr16;
 
-endmodule // FP16RAddSubS1Of4
+   assign ret_0 = x[15];
+   assign ret_1 = y[15];
+   assign ret_2 = xrn;
+   assign ret_3 = yrn;
+   assign ret_4 = xe;
+   assign ret_5 = xn;
+   assign ret_6 = yn;
 
-module FP16RAddSubS2Of4(
+endmodule // FP16RAddSubS1Of5
+
+module FP16RAddSubS2Of5(
  input 	       clk,
  input 	       rst,
- input [11:0]  arg_0, // xf
- input [20:0]  arg_1, // yf
- input [4:0]   arg_2, // xe
- input 	       arg_3, // is_sub
- output [4:0]  ret_0,
- output [21:0] ret_1,
- output        ret_2
-);
-   wire [21:0] r;
-   wire [4:0]  xe;
-   wire [11:0] xf;
-   wire [20:0] yf;
-   wire        is_sub;
+ input 	       arg_0,
+ input 	       arg_1,
+ input [21:0]  arg_2,
+ input [21:0]  arg_3,
+ input [4:0]   arg_4,
+ input 	       arg_5,
+ input 	       arg_6,
+ output [21:0] ret_0,
+ output        ret_1,
+ output        ret_2,
+ output [4:0]  ret_3,
+ output        ret_4,
+ output        ret_5);
 
-   assign xf = arg_0;
-   assign yf = arg_1;
-   assign xe = arg_2;
-   assign is_sub = arg_3;
-   assign r = {1'b0, xf, 10'b0} + {1'b0 + yf};
-
-   assign ret_0 = xe;
-   assign ret_1 = r[21:0];
-   assign ret_2 = is_sub;
+   wire        xn;
+   wire        yn;
+   wire [21:0] rxy;
+   wire        diff_sign;
+   wire [21:0] r_final;
    
-endmodule // FP16RAddSubS2Of4
+   
+   assign xn = arg_5;
+   assign yn = arg_6;
+   assign diff_sign = (xn != yn);
 
-module FP16RAddSubS3Of4(
+   assign rxy = arg_2 + arg_3;
+   assign r_final = diff_sign ? (rxy + 1) : rxy;
+
+   assign ret_0 = r_final;
+   assign ret_1 = arg_0;
+   assign ret_2 = arg_1;
+   assign ret_3 = arg_4;
+   assign ret_4 = arg_5;
+   assign ret_5 = arg_6;
+   
+   
+endmodule // FP16RAddSubS2Of5
+
+module FP16RAddSubS3Of5(
  input 	       clk,
  input 	       rst,
- input [4:0]   arg_0, // exp
- input [21:0]  arg_1, // result
- input 	       arg_2, // is_sub
+ input [21:0]  arg_0,
+ input 	       arg_1,
+ input 	       arg_2,
+ input [4:0]   arg_3,
+ input 	       arg_4,
+ input 	       arg_5,
+ output [20:0] ret_0,
+ output        ret_1,
+ output        ret_2,
+ output [4:0]  ret_3,
+ output        ret_4,
+ output        ret_5,
+ output        ret_6);
+
+   wire [21:0] r;
+   wire        xs;
+   wire        ys;
+   wire [4:0]  e;
+   wire        xn;
+   wire        yn;
+   wire        diff_sign;
+   wire        has_carry;
+   wire        neg;
+   wire [20:0] neg_r;
+   wire [20:0] half_r;
+   wire [20:0] r_diff;
+   wire [20:0] r_same;
+   wire [20:0] r_final;
+   wire [4:0]  eplus;
+   wire [4:0]  e_final;
+   
+   
+   assign r = arg_0;
+   assign xs = arg_1;
+   assign ys = arg_2;
+   assign e = arg_3;
+   assign xn = arg_4;
+   assign yn = arg_5;
+   assign diff_sign = (xn != yn);
+   assign with_carry = r[21:0];
+   assign neg_r = (~r) + 1;
+   assign half_r = r[21:1];
+   assign neg = diff_sign & !with_carry;
+   assign eplus = e + 1;
+
+   assign r_diff = with_carry ? r[20:0] : neg_r;
+   assign r_same = with_carry ? half_r : r[20:0];
+   assign r_final = diff_sign ? r_diff : r_same;
+   assign e_final = (!diff_sign && with_carry) ? eplus : e;
+
+   assign ret_0 = r_final;
+   assign ret_1 = xs;
+   assign ret_2 = ys;
+   assign ret_3 = e_final;
+   assign ret_4 = neg;
+   assign ret_5 = xn;
+   assign ret_6 = yn;
+   
+endmodule // FP16RAddSubS3Of5
+
+module FP16RAddSubS4Of5(
+ input 	       clk,
+ input 	       rst,
+ input [20:0]  arg_0,
+ input 	       arg_1,
+ input 	       arg_2,
+ input [4:0]   arg_3,
+ input 	       arg_4,
+ input 	       arg_5,
+ input 	       arg_6,
  output [15:0] ret_0);
 
-   wire        is_sub;
-   wire        has_carry;
-   wire [4:0]  ep;
-   wire [4:0]  ep0;
-
-   assign is_sub = arg_2;
-   assign has_carry = !is_sub && arg_1[21];
-   assign ep = arg_0;
-   assign ep0 = has_carry ? ep + 1 : ep;
-
-   assign ret_0 = {1'b0, ep0, arg_1[21:12]};
-
-   // TODO: Remove leading 0s after subtraction.
+   // TODO: Shift leading 0s after subtraction.
+   wire [21:0] r;
+   wire        xs;
+   wire        ys;
+   wire [4:0]  e;
+   wire        neg;
+   wire        xn;
+   wire        yn;
+   wire [10:0] rr;
+   wire        s;
    
-endmodule // FP16RAddSubS3Of4
+   assign r = arg_0;
+   assign xs = arg_1;
+   assign ys = arg_2;
+   assign e = arg_3;
+   assign neg = arg_4;
+   assign xn = arg_5;
+   assign yn = arg_6;
+   assign rr = r[20:10];
+   assign s = (xn == yn) ? xs : (yn ? (neg ^ xs): (neg ^ ys));
+
+   assign ret_0 = {s, e, rr};
+   
+endmodule // FP16RAddSubS4Of5
