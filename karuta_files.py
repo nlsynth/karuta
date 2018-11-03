3d0cba2079bf559040fdf446b3e2759d792f853c
Yusuke Tabata <tabata.yusuke@gmail.com>
2018-11-03 15:59:28 +0900

    Add missing package files.

--- karuta_files.py
+++ karuta_files.py
@@ -8,7 +8,7 @@ VERSION = "0.3.7wip"
 
 EXTRA = ["README.md", "NEWS", "configure", "Makefile", "config.mk", "karuta", "src/karuta.gyp", "iroha/src/iroha.gyp", "src/fe/parser.ypp", "genparser.py", "karuta_files.py", "docs/LRM.md", "examples/top.karuta", "examples/fp16r.karuta"]
 
-LIB_FILES = ["lib/karuta.in", "lib/default-isynth.karuta", "lib/karuta_wait.v", "lib/karuta_exit.v", "lib/karuta_server.py", "lib/karuta_wrapper.py", "lib/karuta_examples.py","lib/fp/fp16raddsub.v",  "lib/fp/fp16rmul.v", "lib/fp/fp16r.karuta"]
+LIB_FILES = ["lib/karuta.in", "lib/default-isynth.karuta", "lib/karuta_wait.v", "lib/karuta_exit.v", "lib/karuta_server.py", "lib/karuta_wrapper.py", "lib/karuta_examples.py","lib/fp/fp16raddsub.v",  "lib/fp/fp16rmul.v", "lib/fp/fp16r.karuta", "lib/fp/fp16b.karuta", "lib/fp/fp16baddsub.v",  "lib/fp/fp16bmul.v"]
 
 ifiles = iroha_files.GetGypFileList("iroha/src/iroha.gyp")
 iextras = iroha_files.GetExtraFileList("iroha/")
