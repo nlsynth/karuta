fd3ef9262da1f0160a167580e4ec8898befb39bb
Yusuke Tabata <tabata.yusuke@gmail.com>
2019-01-11 21:03:13 +0900

    Update copyright year to 2019.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) Yusuke Tabata 2007-2018
+// Copyright (C) Yusuke Tabata 2007-2019
 /*
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
