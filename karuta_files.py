9dafcf75f22a99ff1069ec507e74b1a7c9932c71
Yusuke Tabata <tabata.yusuke@gmail.com>
2019-05-29 23:08:08 +0900

    v0.4.3wip

--- karuta_files.py
+++ karuta_files.py
@@ -4,7 +4,7 @@ import iroha_files
 sys.path.append("tests/")
 import test_files
 
-VERSION = "0.4.2"
+VERSION = "0.4.3wip"
 
 EXTRA = ["README.md", "NEWS", "configure", "Makefile", "config.mk", "karuta", "src/karuta.gyp", "iroha/src/iroha.gyp", "src/fe/parser.ypp", "genparser.py", "karuta_files.py", "examples/top.karuta", "examples/fp16r.karuta"]
 
