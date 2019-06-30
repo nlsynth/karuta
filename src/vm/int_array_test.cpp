5dd77a67238ebe64d3036d8f418657bf38b20968
Yusuke Tabata <tabata.yusuke@gmail.com>
2019-06-30 22:52:16 +0900

    Add more tests to IntArray

--- src/vm/int_array_test.cpp
+++ src/vm/int_array_test.cpp
@@ -7,11 +7,22 @@ namespace vm {
 
 void TestIntArray() {
   iroha::NumericWidth w;
+  w.SetWidth(16);
   vector<uint64_t> shape;
   shape.push_back(16);
   IntArray *a = IntArray::Create(w, shape);
 
   TestUtil::Assert(a->GetAddressWidth() == 4);
+  iroha::NumericWidth t;
+  iroha::NumericValue v;
+  v.SetValue0(0x12345678);
+  a->WriteSingle(0, t, v);
+  v = a->ReadSingle(0);
+  TestUtil::Assert((v.GetValue0() & 0xffff) == 0x5678);
+  v = a->ReadSingle(1);
+  TestUtil::Assert(v.GetValue0() == 0);
+  iroha::Numeric n = a->ReadWide(0, 32);
+  TestUtil::Assert(n.type_.GetWidth() == 32);
 }
 
 }
