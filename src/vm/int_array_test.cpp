2d2a5eed67ab529ee46a8a6ea32e97a38a8df7c3
Yusuke Tabata <tabata.yusuke@gmail.com>
2019-07-01 23:14:36 +0900

    Add more tests to IntArray.

--- src/vm/int_array_test.cpp
+++ src/vm/int_array_test.cpp
@@ -10,19 +10,37 @@ void TestIntArray() {
   w.SetWidth(16);
   vector<uint64_t> shape;
   shape.push_back(16);
-  IntArray *a = IntArray::Create(w, shape);
+  std::unique_ptr<IntArray> a(IntArray::Create(w, shape));
 
-  TestUtil::Assert(a->GetAddressWidth() == 4);
-  iroha::NumericWidth t;
-  iroha::NumericValue v;
-  v.SetValue0(0x12345678);
-  a->WriteSingle(0, t, v);
-  v = a->ReadSingle(0);
-  TestUtil::Assert((v.GetValue0() & 0xffff) == 0x5678);
-  v = a->ReadSingle(1);
-  TestUtil::Assert(v.GetValue0() == 0);
-  iroha::Numeric n = a->ReadWide(0, 32);
-  TestUtil::Assert(n.type_.GetWidth() == 32);
+  {
+    TestUtil::Assert(a->GetAddressWidth() == 4);
+    iroha::NumericWidth dw = a->GetDataWidth();
+    TestUtil::Assert(dw.GetWidth() == 16);
+  }
+  {
+    iroha::NumericWidth t;
+    iroha::NumericValue v;
+    v.SetValue0(0x12345678);
+    a->WriteSingle(0, t, v);
+    v = a->ReadSingle(0);
+    TestUtil::Assert((v.GetValue0() & 0xffff) == 0x5678);
+    v = a->ReadSingle(1);
+    TestUtil::Assert(v.GetValue0() == 0);
+  }
+  {
+    iroha::NumericWidth t;
+    iroha::NumericValue v;
+    t.SetWidth(32);
+    v.SetValue0(0x12345678);
+    a->WriteWide(0x200, t, v);
+    iroha::Numeric n = a->ReadWide(0x200, 32);
+    TestUtil::Assert(n.type_.GetWidth() == 32);
+    TestUtil::Assert(n.GetValue0() == 0x12345678);
+    v = a->ReadSingle(0x101);
+    TestUtil::Assert(v.GetValue0() == 0x1234);
+    v = a->ReadSingle(0x100);
+    TestUtil::Assert(v.GetValue0() == 0x5678);
+  }
 }
 
 }
