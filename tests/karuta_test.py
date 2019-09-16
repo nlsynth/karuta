8e9d070b3d0269e24be3355c509235f3c082f3ac
Yusuke Tabata <tabata.yusuke@gmail.com>
2019-09-16 23:56:21 +0900

    Move KarutaTest methods to global.

--- tests/karuta_test.py
+++ tests/karuta_test.py
@@ -8,142 +8,148 @@ tmp_prefix = "/tmp"
 default_tb="test_tb.v"
 verilog_compiler="iverilog"
 
+
+def CheckLog(fn):
+    num_fails = 0
+    done_stat = 0
+    ifh = open(fn, "r")
+    for line in ifh:
+        if re.search("ASSERTION FAILURE", line):
+            num_fails = num_fails + 1
+        if re.search("ASSERTION UNDEFINED", line):
+            num_fails = num_fails + 1
+        if re.search("KARUTA DONE", line):
+            done_stat = 1
+            if re.search("error", line):
+                num_fails = num_fails + 1
+    return {"num_fails":num_fails,
+            "done_stat":done_stat}
+
+
+def ReadTestInfo(fn):
+    test_info = {"exp_fails":0,
+                 "vl_exp_fails":0,
+                 "karuta_ignore_errors":0,
+                 "exp_abort":0}
+    dn = os.path.dirname(fn)
+    if dn != "":
+        dn = dn + "/"
+    test_info["dirname"] = dn
+    ifh = open(fn, "r")
+    for line in ifh:
+        m = re.search("KARUTA_EXPECTED_ERRORS: (\d+)", line)
+        if m:
+            test_info["exp_fails"] = int(m.group(1))
+        m = re.search("VERILOG_EXPECTED_ERRORS: (\d+)", line)
+        if m:
+            test_info["vl_exp_fails"] = int(m.group(1))
+        m = re.search("VERILOG_OUTPUT: (\S+)", line)
+        if m:
+            test_info["verilog"] = m.group(1)
+        m = re.search("VERILOG_TB: (\S+)", line)
+        if m:
+            test_info["verilog_tb"] = m.group(1)
+        m = re.search("KARUTA_IGNORE_ERRORS:", line)
+        if m:
+            test_info["karuta_ignore_errors"] = 1
+        m = re.search("KARUTA_TIMEOUT: (\d+)", line)
+        if m:
+            test_info["karuta_timeout"] = int(m.group(1))
+        m = re.search("KARUTA_EXPECT_ABORT:", line)
+        if m:
+            test_info["exp_abort"] = 1
+        m = re.search("KARUTA_SPLIT_TEST: (\S+)", line)
+        if m:
+            test_info["split_info"] = m.group(1)
+    return test_info
+
+
+def GetVerilogCompileCommand(dut_fn, tb_fn, test_bin_fn):
+    ifh = open(dut_fn, "r")
+    has_mem_if = False
+    has_iroha_mem_if = False
+    has_iroha_sram = False
+    for line in ifh:
+        m = re.search("EXTERNAL_RAM", line)
+        if m:
+            has_mem_if = True
+        m = re.search("sram_addr", line)
+        if m:
+            has_iroha_mem_if = True
+        m = re.search("SRAM_", line)
+        if m:
+            has_iroha_sram = True
+    cmd = (verilog_compiler + " -o " + test_bin_fn + " " +
+           dut_fn + " " + tb_fn + " -I../lib")
+    if has_iroha_sram:
+        has_iroha_mem_if = False
+    if has_mem_if:
+        cmd += " -D KARUTA_RAM"
+        cmd += " -D KARUTA_RAM1"
+    if has_iroha_mem_if:
+        cmd += " -D KARUTA_RAM"
+        cmd += " -D KARUTA_RAM2"
+    return cmd
+
+
+def CheckVerilog(fn, source_fn, summary, test_info):
+    test_bin_fn = tempfile.mktemp()
+    test_log_fn = tempfile.mktemp()
+    if "verilog_tb" in test_info:
+        tb_fn = test_info["dirname"] + test_info["verilog_tb"]
+    else:
+        tb_fn = default_tb
+    cmd = GetVerilogCompileCommand(fn, tb_fn, test_bin_fn)
+    print("  compiling " + fn + "(" + cmd + ")")
+    os.system(cmd)
+    if not os.path.isfile(test_bin_fn):
+        summary.AddVerilogCompileFailure(source_fn)
+        return
+    test_cmd = test_bin_fn + ">" + test_log_fn
+    print ("  running verilog executable " + test_bin_fn +
+           "(" + test_cmd + ")")
+    os.system(test_cmd)
+    res = CheckLog(test_log_fn)
+    num_fails = res["num_fails"]
+    exp_fails = test_info["vl_exp_fails"]
+    summary.AddVerilogResult(source_fn, num_fails,
+                             test_info["karuta_ignore_errors"],
+                             exp_fails)
+    try:
+        os.unlink(test_bin_fn)
+        os.unlink(test_log_fn)
+    except:
+        pass
+
+
+def GetKarutaCommand(source_fn, tf, test_info):
+    vanilla = "--vanilla"
+    if "verilog" in test_info:
+        # verilog tests requires imported modules.
+        vanilla = ""
+    timeout = "1000"
+    if "karuta_timeout" in test_info:
+        timeout = str(test_info["karuta_timeout"])
+    cmd = "KARUTA_DIR=../lib "
+    cmd += karuta_binary + " " + source_fn + " " + vanilla
+    if iroha_binary != "":
+        cmd += " --iroha_binary " + iroha_binary
+    cmd += " --root " + tmp_prefix
+    cmd += " --timeout " + timeout + " "
+    cmd += " --print_exit_status "
+    cmd += " --module_prefix=mod "
+    cmd += " > " + tf
+    return cmd
+
+
 class KarutaTest():
     def __init__(self, source_fn):
         self.source_fn = source_fn
 
-    def CheckLog(self, fn):
-        num_fails = 0
-        done_stat = 0
-        ifh = open(fn, "r")
-        for line in ifh:
-            if re.search("ASSERTION FAILURE", line):
-                num_fails = num_fails + 1
-            if re.search("ASSERTION UNDEFINED", line):
-                num_fails = num_fails + 1
-            if re.search("KARUTA DONE", line):
-                done_stat = 1
-                if re.search("error", line):
-                    num_fails = num_fails + 1
-        return {"num_fails":num_fails,
-                "done_stat":done_stat}
-
-    def ReadTestInfo(self, fn):
-        test_info = {"exp_fails":0,
-                     "vl_exp_fails":0,
-                     "karuta_ignore_errors":0,
-                     "exp_abort":0}
-        dn = os.path.dirname(fn)
-        if dn != "":
-            dn = dn + "/"
-        test_info["dirname"] = dn
-        ifh = open(fn, "r")
-        for line in ifh:
-            m = re.search("KARUTA_EXPECTED_ERRORS: (\d+)", line)
-            if m:
-                test_info["exp_fails"] = int(m.group(1))
-            m = re.search("VERILOG_EXPECTED_ERRORS: (\d+)", line)
-            if m:
-                test_info["vl_exp_fails"] = int(m.group(1))
-            m = re.search("VERILOG_OUTPUT: (\S+)", line)
-            if m:
-                test_info["verilog"] = m.group(1)
-            m = re.search("VERILOG_TB: (\S+)", line)
-            if m:
-                test_info["verilog_tb"] = m.group(1)
-            m = re.search("KARUTA_IGNORE_ERRORS:", line)
-            if m:
-                test_info["karuta_ignore_errors"] = 1
-            m = re.search("KARUTA_TIMEOUT: (\d+)", line)
-            if m:
-                test_info["karuta_timeout"] = int(m.group(1))
-            m = re.search("KARUTA_EXPECT_ABORT:", line)
-            if m:
-                test_info["exp_abort"] = 1
-            m = re.search("KARUTA_SPLIT_TEST: (\S+)", line)
-            if m:
-                test_info["split_info"] = m.group(1)
-        return test_info
-
-    def GetVerilogCompileCommand(self, dut_fn, tb_fn, test_bin_fn):
-        ifh = open(dut_fn, "r")
-        has_mem_if = False
-        has_iroha_mem_if = False
-        has_iroha_sram = False
-        for line in ifh:
-            m = re.search("EXTERNAL_RAM", line)
-            if m:
-                has_mem_if = True
-            m = re.search("sram_addr", line)
-            if m:
-                has_iroha_mem_if = True
-            m = re.search("SRAM_", line)
-            if m:
-                has_iroha_sram = True
-        cmd = (verilog_compiler + " -o " + test_bin_fn + " " +
-               dut_fn + " " + tb_fn + " -I../lib")
-        if has_iroha_sram:
-            has_iroha_mem_if = False
-        if has_mem_if:
-            cmd += " -D KARUTA_RAM"
-            cmd += " -D KARUTA_RAM1"
-        if has_iroha_mem_if:
-            cmd += " -D KARUTA_RAM"
-            cmd += " -D KARUTA_RAM2"
-        return cmd
-
-    def CheckVerilog(self, fn, summary, test_info):
-        test_bin_fn = tempfile.mktemp()
-        test_log_fn = tempfile.mktemp()
-        if "verilog_tb" in test_info:
-            tb_fn = test_info["dirname"] + test_info["verilog_tb"]
-        else:
-            tb_fn = default_tb
-        cmd = self.GetVerilogCompileCommand(fn, tb_fn, test_bin_fn)
-        print("  compiling " + fn + "(" + cmd + ")")
-        os.system(cmd)
-        if not os.path.isfile(test_bin_fn):
-            summary.AddVerilogCompileFailure(self.source_fn)
-            return
-        test_cmd = test_bin_fn + ">" + test_log_fn
-        print ("  running verilog executable " + test_bin_fn +
-               "(" + test_cmd + ")")
-        os.system(test_cmd)
-        res = self.CheckLog(test_log_fn)
-        num_fails = res["num_fails"]
-        exp_fails = test_info["vl_exp_fails"]
-        summary.AddVerilogResult(self.source_fn, num_fails,
-                                 test_info["karuta_ignore_errors"],
-                                 exp_fails)
-        try:
-            os.unlink(test_bin_fn)
-            os.unlink(test_log_fn)
-        except:
-            pass
-
-    def GetKarutaCommand(self, tf, test_info):
-        vanilla = "--vanilla"
-        if "verilog" in test_info:
-            # verilog tests requires imported modules.
-            vanilla = ""
-        timeout = "1000"
-        if "karuta_timeout" in test_info:
-            timeout = str(test_info["karuta_timeout"])
-        cmd = "KARUTA_DIR=../lib "
-        cmd += karuta_binary + " " + self.source_fn + " " + vanilla
-        if iroha_binary != "":
-            cmd += " --iroha_binary " + iroha_binary
-        cmd += " --root " + tmp_prefix
-        cmd += " --timeout " + timeout + " "
-        cmd += " --print_exit_status "
-        cmd += " --module_prefix=mod "
-        cmd += " > " + tf
-        return cmd
-
     def RunTest(self, summary):
-        test_info = self.ReadTestInfo(self.source_fn)
+        test_info = ReadTestInfo(self.source_fn)
         tf = tempfile.mktemp()
-        cmd = self.GetKarutaCommand(tf, test_info)
+        cmd = GetKarutaCommand(self.source_fn, tf, test_info)
         if "verilog" in test_info:
             try:
                 os.unlink(tmp_prefix + "/" + test_info["verilog"])
@@ -157,9 +163,10 @@ class KarutaTest():
         if rv:
             summary.AddAbort(rv)
         if rv == 0 and "verilog" in test_info:
-            self.CheckVerilog(tmp_prefix + "/" + test_info["verilog"],
-                              summary, test_info)
-        res = self.CheckLog(tf)
+            CheckVerilog(tmp_prefix + "/" + test_info["verilog"],
+                         self.source_fn,
+                         summary, test_info)
+        res = CheckLog(tf)
         num_fails = res["num_fails"]
         done_stat = res["done_stat"]
         exp_fails = test_info["exp_fails"]
