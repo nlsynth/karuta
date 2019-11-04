a695df170e0ededb7bed1fb73206ef29ca712ab8
Yusuke Tabata <tabata.yusuke@gmail.com>
2019-11-04 21:40:55 +0900

    Initial code to generate examples.

--- lib/lib_files.py
+++ lib/lib_files.py
@@ -1,5 +1,11 @@
 #
-LIB_FILES = ["karuta.in", "default-isynth.karuta", "karuta_wait.v", "karuta_exit.v", "karuta_server.py", "karuta_wrapper.py", "karuta_examples.py","fp/fp16raddsub.v",  "fp/fp16rmul.v", "fp/fp16r.karuta", "fp/fp16b.karuta", "fp/fp16baddsub.v",  "fp/fp16bmul.v"]
+BASE_FILES = ["karuta.in", "default-isynth.karuta", "karuta_wait.v", "karuta_exit.v"]
+
+FP_LIB_FILES = ["fp/fp16raddsub.v", "fp/fp16rmul.v", "fp/fp16r.karuta", "fp/fp16b.karuta", "fp/fp16baddsub.v", "fp/fp16bmul.v"]
+
+PLAY_GROUND_SERVER = ["karuta_server.py", "karuta_wrapper.py", "karuta_examples.py", "make_examples.py"]
+
+LIB_FILES = BASE_FILES + FP_LIB_FILES + PLAY_GROUND_SERVER
 
 def GetFiles(base_dir):
     files = []
---
+++ lib/make_examples.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# collects files under examples
+
+import glob
+import os
+
+files = glob.glob("example_srcs/*.karuta")
+
+m = {}
+
+def proc(fn):
+    desc = ""
+    src = ""
+    k = os.path.basename(fn)
+    index = -1
+    for line in open(fn):
+        if line.startswith("##"):
+            line = line[:-1]
+            t = line.find("key:")
+            if t > -1:
+                k = line[t + 4:]
+                continue
+            t = line.find("index:")
+            if t > -1:
+                index = int(line[t + 6:])
+                continue
+            desc = line
+            continue
+        src = src + line
+    if desc == "":
+        return (None, None)
+    v = {}
+    v['s'] = src
+    if desc:
+        v['n'] = desc[3:]
+    else:
+        v['n'] = ''
+    if index > -1:
+        v['i'] = str(index)
+    return (k, v)
+
+index = 100
+
+for fn in files:
+    (k, v) = proc(fn)
+    if not k:
+        continue
+    if not 'i' in v:
+        v['i'] = str(index)
+    m[k] = v
+    index = index + 1
+
+
+print("# built with make_examples.py")
+print("EXAMPLES =" + str(m))
