13362b036bc6bab08c89006d12431e5fc2d7b9e8
Yusuke Tabata <tabata.yusuke@gmail.com>
2020-10-03 11:41:45 +0900

    v0.6.1

--- karuta_files.py
+++ karuta_files.py
@@ -1,16 +1,18 @@
+import lib_files
+import test_files
+import iroha_files
 import sys
 sys.path.append("iroha/")
-import iroha_files
 sys.path.append("tests/")
-import test_files
 sys.path.append("lib/")
-import lib_files
 
-VERSION = "0.6.1wip"
+VERSION = "0.6.1"
 
-EXTRA = ["README.md", "NEWS", "configure", "Makefile", "config.mk", "karuta", "src/karuta.gyp", "iroha/src/iroha.gyp", "src/fe/parser.ypp", "genparser.py", "karuta_files.py", "examples/top.karuta", "examples/fp16r.karuta"]
+EXTRA = ["README.md", "NEWS", "configure", "Makefile", "config.mk", "karuta", "src/karuta.gyp", "iroha/src/iroha.gyp",
+         "src/fe/parser.ypp", "genparser.py", "karuta_files.py", "examples/top.karuta", "examples/fp16r.karuta"]
 
-DOCS = ["docs/index.rst", "docs/introduction.rst", "docs/reference.rst", "docs/usersguide.rst", "docs/designandimpl.rst", "docs/experimental.rst"]
+DOCS = ["docs/index.rst", "docs/introduction.rst", "docs/reference.rst",
+        "docs/usersguide.rst", "docs/designandimpl.rst", "docs/experimental.rst"]
 
 ifiles = iroha_files.GetGypFileList("iroha/src/iroha.gyp")
 iextras = iroha_files.GetExtraFileList("iroha/")
